2aa4db919b873a45af68e4770ee42b4bb7978bce
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-02-02 23:34:56 +0900

    Add 301 and 302

--- duo3.0-english_sentences-301_400.rb
+++ duo3.0-english_sentences-301_400.rb
@@ -1,2 +1,15 @@
 album 'DUO 3.0 English Sentences 301-400' do
+  track '301' do
+    directory 3
+    file 47
+    start 35
+    duration 5
+  end
+
+  track '302' do
+    directory 3
+    file 48
+    start 6.4
+    duration 6
+  end
 end
