9ec1c2912dfcaa6dc9193aa8ee387fed8d75ba74
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-02-02 23:37:32 +0900

    Add 401 and 402

--- duo3.0-english_sentences-401_500.rb
+++ duo3.0-english_sentences-401_500.rb
@@ -1,2 +1,15 @@
 album 'DUO 3.0 English Sentences 401-500' do
+  track '401' do
+    directory 4
+    file 38
+    start 8
+    duration 6
+  end
+
+  track '402' do
+    directory 4
+    file 38
+    start 38
+    duration 6
+  end
 end
