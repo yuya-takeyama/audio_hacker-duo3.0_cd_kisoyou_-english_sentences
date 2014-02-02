8efbc65151ee5b967b4a82ffa723afe0fd6c47a7
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-02-02 23:41:18 +0900

    Add 501 and 502

--- duo3.0-english_sentences-501_560.rb
+++ duo3.0-english_sentences-501_560.rb
@@ -1,2 +1,15 @@
 album 'DUO 3.0 English Sentences 501-560' do
+  track '501' do
+    directory 5
+    file 28
+    start 31
+    duration 6
+  end
+
+  track '502' do
+    directory 5
+    file 29
+    start 7.2
+    duration 5.2
+  end
 end
