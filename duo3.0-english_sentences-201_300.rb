6c0ccb7aab082145cecbce09297875a49979d6cc
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-02-02 23:31:25 +0900

    Add 201 and 202

--- duo3.0-english_sentences-201_300.rb
+++ duo3.0-english_sentences-201_300.rb
@@ -1,2 +1,15 @@
 album 'DUO 3.0 English Sentences 201-300' do
+  track '201' do
+    directory 2
+    file 53
+    start 46
+    duration 8
+  end
+
+  track '202' do
+    directory 2
+    file 54
+    start 6
+    duration 5
+  end
 end
